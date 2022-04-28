da2b61121f69e2b7a4a8babe6ea134671401c69c
kamaeff <kamaeff2@gmail.com>
2022-04-28 12:47:43 +0300

    Сделана анимация лайка и подсчет в файле js

--- Script/myscript.js
+++ Script/myscript.js
@@ -1,7 +1,8 @@
-function Click(){
-  updateLike(++counter);
+let counter = 0;
+function Click() {
+    updateLike(counter=1);
 }
 
-function updateLike(val){
-  document.getElementById("label").innerHTML = val;
+function updateLike(val) {
+    document.getElementById("label").innerHTML = val;
 }
