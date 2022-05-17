35eff705ea14519457926fec6423e119809e6ed7
kamaeff <kamaeff2@gmail.com>
2022-05-17 17:13:17 +0300

    Сделана анимация лайка

--- Script/myscript.js
+++ Script/myscript.js
@@ -1,53 +1,3 @@
-let counter = 0;
-
-function valid()
-{
-    let form = document.querySelector('.header__form_registr')
-    let search = form.querySelectorAll('.search')
-    let errors = form.querySelectorAll('.error');
-    let password = form.querySelector('.search--password')
-    let passwordConfirmation= form.querySelector('.search--password-confirmation')
-    let flag =true
-
-    if(errors){
-        for(let i=0; i<errors.length; i++)
-        {       
-            errors[i].remove()
-            flag = false
-        }
-    }
-
-    for(let i=0;i<search.length;i++)
-    {
-        if(!search[i].value)
-        {
-            let error = document.createElement('div')
-            error.className = 'error'
-            error.style.color = 'red'
-            error.style.fontSize = '12px'
-            error.innerHTML = 'Заполните поле'
-            form[i].parentElement.insertBefore(error, search[i])
-            flag = false
-        }
-    }
-
-    if(password.value != passwordConfirmation.value)
-    {
-        let error = document.createElement('div')
-        error.className = 'error'
-        error.style.color = 'red'
-        error.innerHTML = 'Пароли не совпадают'
-        password.parentElement.insertBefore(error, password)
-        flag =false
-    }
-    if(flag)
-    {
-        alert("Регистрация прошла успешна")
-    
-    }
-}
-
-
 function openSection(idSection) {
     closeSection();
     document.getElementById(idSection).style.display = "block";
@@ -62,17 +12,12 @@ function closeSection()
     }
     document.body.style.overflow = 'visible';
 }
-
-function Click() {
-    updateLike(counter=1);
-}
-
-function updateLike(val) {
-    document.getElementById("label").innerHTML = val;
-
-}
-
+var val = 0;
 var like = document.getElementById("like");
-like.addEventListener("click", function(){
-    this.classList.add("active");
+like.addEventListener("click", function(){ 
+    this.classList.toggle("active");
+    document.getElementById("label").innerHTML = val = 1 - val;
+
 });
+
+let inputIn = document.querySelector('.header__form_registr');
