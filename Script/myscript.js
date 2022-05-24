2209babf0ba3cd62610679a29544bac3b5d3e687
kamaeff <kamaeff2@gmail.com>
2022-05-24 22:36:03 +0300

    Сделанна обработка пустых форм

--- Script/myscript.js
+++ Script/myscript.js
@@ -6,12 +6,13 @@ function openSection(idSection) {
 function closeSection()
 {
     let content;
-    content = document.getElementsByClassName("header__form");
+    content = document.getElementsByClassName("mainform");
     for (i = 0; i < content.length; i++) {
         content[i].style.display = "none";
     }
     document.body.style.overflow = 'visible';
 }
+
 let val = 0;
 let like = document.getElementById("like");
 like.addEventListener("click", function(){ 
@@ -19,3 +20,20 @@ like.addEventListener("click", function(){
     document.getElementById("label").innerHTML = val = 1 - val;
 
 });
+
+let form = document.querySelector('.js-form'),
+    formInputs = document.querySelectorAll('.js-input');
+
+
+form.onsubmit = function(){
+
+    formInputs.forEach(function (input) {
+        if (input.value === '') {
+            input.classList.add('error');
+
+        } else {
+            input.classList.remove('error');
+        }
+    });
+    return false;
+}
