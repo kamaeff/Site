fa22ad8bfb954eb9a61811695f0445701302efea
kamaeff <kamaeff2@gmail.com>
2022-05-30 13:25:38 +0300

    Сделана валидация формы регистрации: сделана проверка на пустоту, ставнение паролей, проверка паролей на 8 сиволов и проверка паролей на английские символы

--- Script/myscript.js
+++ Script/myscript.js
@@ -1,3 +1,4 @@
+//Open and close section
 function openSection(idSection) {
     closeSection();
     document.getElementById(idSection).style.display = "block";
@@ -13,6 +14,7 @@ function closeSection()
     document.body.style.overflow = 'visible';
 }
 
+//like animation
 let val = 0;
 let like = document.getElementById("like");
 like.addEventListener("click", function(){ 
@@ -21,19 +23,62 @@ like.addEventListener("click", function(){
 
 });
 
+//input protector
 let form = document.querySelector('.js-form'),
     formInputs = document.querySelectorAll('.js-input');
+let pass = document.getElementById('password'),
+    repass = document.getElementById('repassword'),
+    login = document.getElementById('login');
 
+let re = new RegExp('^[a-zA-Z0-9]+$');
 
 form.onsubmit = function(){
-
-    formInputs.forEach(function (input) {
-        if (input.value === '') {
-            input.classList.add('error');
-
-        } else {
+    formInputs.forEach(function (input) {  
+        if (input.value === '') {   
+            input.classList.add('error');  
+            input.classList.remove('pos'); 
+            console.log('inputs none');
+            if (pass.value === '' || repass.value === ''){
+                document.getElementById('message').innerHTML = 'Пароли не могут быть пустыми!';
+                return false;
+            }
+        }else{ 
             input.classList.remove('error');
-        }
+            input.classList.add('pos');
+        }  
     });
-    return false;
+    if (pass.value.length < 8 ){
+        document.getElementById("message").innerHTML = "Пароль должен быть не менее 8 символов";
+        console.log('not 8 symbols');
+        pass.classList.remove('pos');
+        repass.classList.remove('pos');
+        pass.classList.add('error');
+        repass.classList.add('error');
+        return false;  
+    }
+
+    if(!re.test(pass.value)){
+        pass.classList.remove('pos');
+        repass.classList.remove('pos');
+        pass.classList.add('error');
+        repass.classList.add('error');
+        console.log('not english words');
+        document.getElementById('message').innerHTML = 'Пароль не содержит английский буквы!';
+        return false; 
+    }
+     
+    if ((pass.value == repass.value) && pass.value != '' && repass.value != '' && input.value != ''){     
+        console.log('Yes');
+        document.getElementById('message').innerHTML = '';
+        form.submit();
+    }
+    else {
+        console.log('No');
+        pass.classList.remove('pos');
+        repass.classList.remove('pos');
+        pass.classList.add('error');
+        repass.classList.add('error');
+        document.getElementById('message').innerHTML = 'Пароли не совпадают!';
+        return false;
+    } 
 }
